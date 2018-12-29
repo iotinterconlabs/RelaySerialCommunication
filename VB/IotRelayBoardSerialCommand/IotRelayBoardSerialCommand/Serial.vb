b703c8462a6125db38b8d6780ac34132041286ce
Anurag <info@iotintercon.com>
2018-12-29 19:10:39 +0530

    Added VB code to control relay board

---
+++ VB/IotRelayBoardSerialCommand/IotRelayBoardSerialCommand/Serial.vb
@@ -0,0 +1,17 @@
+﻿Module Serial
+    Public Function SendSerialData(ByVal data As String, ByVal ComPort As String)
+        Using com As IO.Ports.SerialPort =
+                My.Computer.Ports.OpenSerialPort(ComPort)
+            com.WriteLine(data)
+        End Using
+        Return "0"
+    End Function
+
+
+    Sub Main()
+        Console.Write("Enter Com Port :")
+        Dim ComPortNo As String = Console.ReadLine()
+        Console.Write("Enter argument :")
+        SendSerialData(Console.ReadLine() + "\r\n", ComPortNo) 'sending
+    End Sub
+End Module
